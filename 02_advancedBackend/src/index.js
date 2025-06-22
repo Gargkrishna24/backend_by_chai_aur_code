814ba13ec28e1165ab5de798e5181c134cd8614d
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-22 16:25:20 +0530

    Add .custom API

--- 02_advancedBackend/src/index.js
+++ 02_advancedBackend/src/index.js
@@ -4,7 +4,16 @@ dotenv.config({path:'./env'})
 
 import connectDB from "./db/index.js";
 connectDB()
-
+.then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running at http://localhost:${process.env.PORT}`);
+    
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection failed !!! ",err);
+  
+})
 
 
 
