663dcc0a1d4e1b27407ce1ffe307a4f1dd49826c
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-22 19:18:27 +0530

    make changes

--- 02_advancedBackend/src/Models/user.models.js
+++ 02_advancedBackend/src/Models/user.models.js
@@ -84,4 +84,4 @@ userSchema.methods.generateRefreshToken = function () {
   );
 };
 
-export const User = mongoose.model("USer", userSchema);
+export const User = mongoose.model("User", userSchema);
